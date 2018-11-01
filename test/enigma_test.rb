15af463e9737bb9d2ecc31a52880473f5c4b207f
Ben Lee <bendelonlee@gmail.com>
2018-11-01 11:42:25 -0600

    Writes some tests that won't be hard to pass

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,9 +1,28 @@
 require './test/test_helper'
 require './lib/enigma'
 class EnigmaTest < Minitest::Test
+
+
+  def setup
+    @e = Enigma.new
+  end
+
   def test_it_exists
-    assert_instance_of Enigma, Enigma.new
-    
+    assert_instance_of Enigma, @e
+  end
+
+  def test_it_encrypts
+    skip
+    actual = @e.encrypt("This secret must be kept secret")
+    asssert_equal "Soemthing", actual
+  end
+
+  def test_it_rotates_a_letter
+    asssert_equal 'b', @e.rotate_letter('a', '1')
+  end
+
+  def test_it_rotates_a_string
+    asssert_equal 'bcad', @e.rotate_letter('aaaa', '1203')
   end
 
 end

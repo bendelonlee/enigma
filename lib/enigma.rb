7faaef1b51d387ef651c0cea7e3f7757a9d30f28
Ben Lee <bendelonlee@gmail.com>
2018-11-01 14:33:01 -0600

    Adds offsets

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -13,11 +13,24 @@ class Enigma
     CHARACTER_MAP[(index + amount) % 39]
   end
 
-  def rotate_string(string, amounts)
+  def rotate_string_by_amounts(string, amounts)
     amounts_cycle = amounts.cycle
     string.chars.map do |ch|
       rotate_letter(ch, amounts_cycle.next.to_i)
     end.join
+  end
+
+  def rotate_string_by_key(string, key)
+    rotate_string_by_amounts(string, key_to_amounts(key))
+  end
+
+  def key_to_amounts(key)
+    key.chars.each_cons(2).map { |pair| pair.join.to_i }
+  end
+
+  def key_to_amounts_with_offsets(key, offsets)
+    offsets_cycle = offsets.cycle
+    key_to_amounts(key).map { |n| n + offsets_cycle.next }
   end
 
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -22,10 +22,25 @@ class EnigmaTest < Minitest::Test
     assert_equal 'a', @e.rotate_letter('.', 1)
   end
 
-  def test_it_rotates_a_string
-    assert_equal 'bcad', @e.rotate_string('aaaa', [1, 2, 0, 3])
+  def test_it_rotates_a_string_by_amounts
+    assert_equal 'bcad', @e.rotate_string_by_amounts('aaaa', [1, 2, 0, 3])
 
   end
+
+  def test_it_turns_keys_into_amounts
+    assert_equal [41,15,52,21], @e.key_to_amounts('41521')
+  end
+
+  def test_it_turns_keys_into_amounts
+    assert_equal [41,15,52,21], @e.key_to_amounts('41521')
+  end
+
+  def test_it_turns_keys_and_offsets_into_amounts
+    assert_equal [42,17,55,25], @e.key_to_amounts_with_offsets('41521', [1,2,3,4])
+
+  end
+
+
 
 
 end

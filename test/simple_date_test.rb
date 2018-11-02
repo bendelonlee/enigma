4acd3c1aa539f05aedefce34cc8deeb57630cd79
Ben Lee <bendelonlee@gmail.com>
2018-11-02 17:00:28 -0600

    Two tests written for SimpleDate

--- test/simple_date_test.rb
+++ test/simple_date_test.rb
@@ -4,11 +4,20 @@ require './lib/simple_date'
 class SimpleDateTest < Minitest::Test
 
   def setup
-    @sd = SimpleDate.new
+    @date = Date.new(2013, 06,01)
+    @sd = SimpleDate.new(@date)
   end
 
   def test_it_exists
     assert_instance_of SimpleDate, @sd
+  end
+
+  def test_it_has_a_ddmmyy_string
+    assert_equal '010613', @sd.ddmmyy
+  end
+
+  def test_it_has_a_date_object
+    assert_equal @date, @sd.date
   end
 
 end

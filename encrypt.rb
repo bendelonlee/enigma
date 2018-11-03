70694950b86e64d8cfea1ab19c9975ee73fe563b
Ben Lee <bendelonlee@gmail.com>
2018-11-03 09:03:42 -0600

    It now outputs the key and the date after encrypting

--- encrypt.rb
+++ encrypt.rb
@@ -3,8 +3,13 @@ enigma = Enigma.new
 
 message_file      = File.open(ARGV[0], "r")
 parsed_message    = message_file.readlines.map(&:chomp).join(' ').downcase
-encrypted_message = enigma.encrypt(parsed_message)[:encryption]
+encryption_result = enigma.encrypt(parsed_message)
+encrypted_message = encryption_result[:encryption]
+key               = encryption_result[:key]
+date              = encryption_result[:date]
 new_file_path     = "./data/#{ARGV[1]}"
 new_file          = File.open(new_file_path, "w")
 new_file.write(encrypted_message)
 new_file.close
+
+puts "Created #{new_file_path} with the key #{key} and date #{date}"

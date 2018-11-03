ea1cd181549d35ac9e7e02c06df5f984dbbd49c4
Ben Lee <bendelonlee@gmail.com>
2018-11-03 08:52:20 -0600

    file is written with encrypted text

--- encrypt.rb
+++ encrypt.rb
@@ -1,16 +1,10 @@
 require './lib/enigma'
-message_file = File.open(ARGV[0], "r")
-
-require 'pry'; binding.pry
-
-parsed_message = message_file.readlines.map(&:chomp).join(' ').downcase
 enigma = Enigma.new
 
-
-
-
-new_file_path = "./data/#{ARGV[1]}"
-
-new_file = File.open(new_file_path, "w")
-new_file.write(parsed_message)
+message_file      = File.open(ARGV[0], "r")
+parsed_message    = message_file.readlines.map(&:chomp).join(' ').downcase
+encrypted_message = enigma.encrypt(parsed_message)[:encryption]
+new_file_path     = "./data/#{ARGV[1]}"
+new_file          = File.open(new_file_path, "w")
+new_file.write(encrypted_message)
 new_file.close

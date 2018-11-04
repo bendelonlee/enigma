c66c89080dec69369fab04ac78bdc48091c63ae7
Ben Lee <bendelonlee@gmail.com>
2018-11-03 18:02:09 -0600

    Adds key handling and rotation modules to Cracker class

--- lib/cracker.rb
+++ lib/cracker.rb
@@ -1,11 +1,15 @@
 require_relative 'crypter'
+require_relative 'key_handling'
+require_relative 'rotation'
 
 class Cracker < Crypter
+  include KeyHandling
+  include Rotation
 
   def initialize(string, date)
     @string = string
     @date = date
-    @possible_key = '999'
+    @possible_key_values = (0..9999)
   end
 
   def next_possible_amounts

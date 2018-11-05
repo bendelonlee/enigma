606e9a700a9988c1471e400716ca436925f3496f
Ben Lee <bendelonlee@gmail.com>
2018-11-05 09:52:12 -0700

    Adds test for word_cracking with long string that doesn't have end at its end

--- test/word_cracking_test.rb
+++ test/word_cracking_test.rb
@@ -4,7 +4,8 @@ require './lib/cracker'
 class WordCrackingTest < Minitest::Test
 
   def setup
-    @cracker = Cracker.new('keder ohulwthnw', ReliableDate.new('040895'), nil)
+    @cracker = Cracker.new('mhoztskjkezgfuy tbkgdev tskjkezgbtfcblrggozgynjgpizottnltwuyx kux paltfzh zoutfpm bpelfiy yakeretfuyyvkytskjkez
+', ReliableDate.new('051118'), nil)
   end
 
   def test_it_exists
@@ -12,17 +13,17 @@ class WordCrackingTest < Minitest::Test
   end
 
   def test_when_it_has_no_assumptions_it_can_still_crack_the_encryption_by_checking_for_common_words
-    assert_equal '02715', @cracker.crack
+    assert_equal '11857', @cracker.crack
   end
 
   def test_check_next_possible_amounts_for_word_count
-    2715.times{@cracker.next_possible_key}
-    assert_equal ({'02715'=>3}), @cracker.check_next_possible_amounts_for_word_count
+    70908.times{@cracker.next_possible_key}
+    assert_equal ({'11857'=>4}), @cracker.check_next_possible_amounts_for_word_count
   end
 
 
   def test_method_check_all_possible_amounts_and_find_key_that_results_in_most_words
     skip
-    assert_equal '02715', @cracker.check_all_possible_amounts_and_find_key_that_results_in_most_words
+    assert_equal '11857', @cracker.check_all_possible_amounts_and_find_key_that_results_in_most_words
   end
 end

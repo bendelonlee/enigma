68261b63be10c44406590292e26ca6535f8ea341
Ben Lee <bendelonlee@gmail.com>
2018-11-05 09:05:24 -0700

    Passes test_method_check_all_possible_amounts_and_find_key_that_results_in_most_words

--- test/word_cracking_test.rb
+++ test/word_cracking_test.rb
@@ -23,7 +23,6 @@ class WordCrackingTest < Minitest::Test
 
 
   def test_method_check_all_possible_amounts_and_find_key_that_results_in_most_words
-    skip
     assert_equal '02715', @cracker.check_all_possible_amounts_and_find_key_that_results_in_most_words
   end
 end

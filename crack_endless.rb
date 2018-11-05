4d59c1a9061277a798e00933ea3bce7b5c6829f0
Ben Lee <bendelonlee@gmail.com>
2018-11-05 13:40:32 -0700

    Adds crack_endless.rb which will crack without a message having the word end from the command line

---
+++ crack_endless.rb
@@ -0,0 +1,16 @@
+require './lib/enigma'
+
+enigma = Enigma.new
+
+date              = ARGV[2]
+encrypted_file    = File.open(ARGV[0], "r")
+parsed_encryption = encrypted_file.readlines.map(&:chomp).join(' ').downcase
+decryption_result = enigma.crack_endless(parsed_encryption, date)
+key               = decryption_result[:key]
+decrypted_message = decryption_result[:decryption]
+new_file_path     = "#{ARGV[1]}"
+new_file          = File.open(new_file_path, "w")
+new_file.write(decrypted_message)
+new_file.close
+
+puts "Created #{new_file_path} with the cracked key #{key} and date #{date}"
